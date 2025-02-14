c938ff321732c01ee33af48a25195186fb2485b8
Bimal Pandey <163624611+bimal009@users.noreply.github.com>
2025-02-14 19:39:08 +0545

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+list =[]
+
+m1=int(input("entre students marks"))
+list.append(m1)
+m2=int(input("entre students marks"))
+list.append(m2)
+m3=int(input("entre students marks"))
+list.append(m3)
+
+list.sort()
+
+print(list)
