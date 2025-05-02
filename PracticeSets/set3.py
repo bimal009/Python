486f058446b98105e2052ed96803c07fb17cb8c9
bimal009 <pandeybimal616@gmail.com>
2025-05-02 22:47:16 +0545

    Add programs for storing fruits, sorting marks, tuple immutability check, summing numbers, and counting zeros in a tuple

---
+++ PracticeSets/set3.py
@@ -0,0 +1,83 @@
+# Write a program to store seven fruits in a list entered by the user.
+
+# list=[]
+# fruit_one= input("entre fruit one:")
+# list.append(fruit_one)
+# fruit_two= input("entre fruit two:")
+# list.append(fruit_two)
+
+# fruit_three= input("entre fruit three:")
+# list.append(fruit_three)
+
+# fruit_four= input("entre fruit four:")
+# list.append(fruit_four)
+
+# fruit_five= input("entre fruit five:")
+# list.append(fruit_five)
+
+# fruit_six= input("entre fruit six:")
+# list.append(fruit_six)
+
+# fruit_seven= input("entre fruit seven:")
+# list.append(fruit_seven)
+
+
+# print(list)
+
+# 2. Write a program to accept marks of 6 students and display them in a sorted
+# manner.
+
+# marks=[]
+
+# mark_one= int(input("entre mark one:"))
+# marks.append(mark_one)
+# mark_two= int(input("entre mark two:"))
+# marks.append(mark_two)
+
+# mark_three= int(input("entre mark three:"))
+# marks.append(mark_three)
+
+# mark_four= int(input("entre mark four:"))
+# marks.append(mark_four)
+
+# mark_five= int(input("entre mark five:"))
+# marks.append(mark_five)
+
+# marks.sort()
+# print(marks)
+
+
+# 3. Check that a tuple type cannot be changed in python.
+# a = ("bimal", 1, 22.5)
+# try:
+#     a[0] = "sudeep"
+# except TypeError as e:
+#     print("Error:", e)
+
+# print(a)
+# 4. Write a program to sum a list with 4 numbers.
+
+
+# numbers=[]
+
+# number_one= int(input("entre number one:"))
+# numbers.append(number_one)
+# number_two= int(input("entre number two:"))
+# numbers.append(number_two)
+
+# number_three= int(input("entre number three:"))
+# numbers.append(number_three)
+
+# number_four= int(input("entre number four:"))
+# numbers.append(number_four)
+
+# addition= numbers[0]+numbers[1]+numbers[2]+numbers[3]
+# print("the sum of the numbers is:",addition )
+
+
+# 5. Write a program to count the number of zeros in the following tuple:
+a = (7, 0, 8, 0, 0, 9)
+
+count =a.count(0)
+print("Total number of 0's:", count)
+
