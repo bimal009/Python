b4cf394ceb9959c3fb62dea8a9635d23868d86f8
bimal009 <pandeybimal616@gmail.com>
2025-05-02 23:01:32 +0545

    feat: add dictionary and set operations examples
    - Demonstrated dictionary update and items methods
    - Showed set operations: add, update, pop, and comments on discard, remove, and clear

---
+++ basics/dictionary.py
@@ -0,0 +1,24 @@
+# a={
+#     "name":"bimal",
+#     "age":19,
+#     "height":6.2,
+# }
+# # print(a.get("age"))
+# # print(a.keys())
+# # print(a.values())
+# a.update({"edu":"Bachloer"})
+# print(a.items())
+
+
+s=set()
+s.add(1)
+s.add(2)
+
+s.update([3,4])
+# s.discard(3)
+# s.remove(3)
+# s.clear()\
+poped= s.pop() 
+# // pops the fist element
+print(poped)
+print(s)
--- basics/tempCodeRunnerFile.py
+++
@@ -1,4 +0,0 @@
-friends=["bimal","sohan",34,3.46]
-
-# print(friends[2])
-# print(friends[1:3])
