a5c081163664da6634ddb1e98f2083538317be96
bimal009 <pandeybimal616@gmail.com>
2025-05-02 22:30:11 +0545

    Add programs for greeting user, template letter formatting, double space detection and replacement, and escape sequence formatting

---
+++ PracticeSets/set2.py
@@ -0,0 +1,56 @@
+# Write a python program to display a user entered name followed by Good
+# Afternoon using input () function.
+
+# name = input("Entre Your Name:")
+# print("Good Afternoon",name)
+
+# 2. Write a program to fill in a letter template given below with name and date.
+# letter = '''
+# Dear <|Name|>,
+# You are selected!
+# <|Date|>
+# '''
+
+# name = input("Entre Your Name:")
+# date = input("Entre The Date:")
+# letter = '''
+#  Dear <|Name|>,
+#  You are selected!
+#  <|Date|>
+# '''
+
+# filled_letter= letter.replace("<|Name|>",name).replace(" <|Date|>",date)
+
+
+# print(filled_letter)
+
+
+# 3. Write a program to detect double space in a string.
+
+# text = "This  is  a  string  with  double  spaces."
+
+
+
+# if "  " in text:
+#     print("Found double spaces")
+# else:
+#     print("Not found")
+
+
+# print(text.count("  "))
+
+# 4. Replace the double space from problem 3 with single spaces.
+# text = "This  is  a  string  with  double  spaces."
+
+# fixed_text=text.replace("  "," ")
+# print("Original text:",text)
+# print("Fixed text:",fixed_text)
+
+
+
+# 5. Write a program to format the following letter using escape sequence
+# characters.
+letter = "Dear Harry, \nthis python course is nice.\nThanks!"
+print(letter)
+
+
